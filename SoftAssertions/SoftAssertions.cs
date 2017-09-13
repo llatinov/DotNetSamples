89b31f81bb116db089d31f0f5e0e209640b463d3
Lyudmil Latinov <llatinov@gmail.com>
2017-09-13 15:34:25 +0300

    Add code for SoftAssertions examples.

---
+++ SoftAssertions/SoftAssertions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+
+namespace SoftAssertions
+{
+    public class SoftAssertions
+    {
+        private readonly List<SingleAssert> _verifications = new List<SingleAssert>();
+
+        public void Add(string message, string expected, string actual)
+        {
+            _verifications.Add(new SingleAssert(message, expected, actual));
+        }
+
+        public void Add(string message, bool expected, bool actual)
+        {
+            Add(message, expected.ToString(), actual.ToString());
+        }
+
+        public void Add(string message, int expected, int actual)
+        {
+            Add(message, expected.ToString(), actual.ToString());
+        }
+
+        public void AddTrue(string message, bool actual)
+        {
+            _verifications.Add(new SingleAssert(message, true.ToString(), actual.ToString()));
+        }
+
+        public void AssertAll()
+        {
+            var failed = _verifications.Where(v => v.Failed).ToList();
+            // TODO: Customise verification of add one from your favourite unit testing framework
+            failed.Should().BeEmpty();
+        }
+
+        private class SingleAssert
+        {
+            private readonly string _message;
+            private readonly string _expected;
+            private readonly string _actual;
+
+            public bool Failed => _expected != _actual;
+
+            public SingleAssert(string message, string expected, string actual)
+            {
+                _message = message;
+                _expected = expected;
+                _actual = actual;
+            }
+
+            public override string ToString()
+            {
+                return $"'{_message}' assert was expected to be '{_expected}' but was '{_actual}'";
+            }
+        }
+    }
+}
---
+++ SoftAssertions/UnitTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SoftAssertions
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private SoftAssertions _softAssertions;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _softAssertions = new SoftAssertions();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _softAssertions.AssertAll();
+        }
+
+        [TestMethod]
+        public void TestPassingSoftAssertions()
+        {
+            _softAssertions.Add("Passing Add assertion", true, true);
+            _softAssertions.AddTrue("Passing AddTrue assertion", true);
+        }
+
+        [TestMethod]
+        public void TestFalingSoftAssertions()
+        {
+            _softAssertions.Add("Failing Add assertion", true, false);
+            _softAssertions.AddTrue("Failing AddTrue assertion", false);
+        }
+
+        [TestMethod]
+        public void TestMixedSoftAssertions()
+        {
+            _softAssertions.Add("Passing Add assertion", true, true);
+            _softAssertions.Add("Failing Add assertion", true, false);
+            _softAssertions.AddTrue("Passing AddTrue assertion", true);
+            _softAssertions.AddTrue("Failing AddTrue assertion", false);
+        }
+    }
+}
