55fdc1788b2538ee7e7c5c8102044de4af821ed3
Lyudmil Latinov <llatinov@gmail.com>
2017-09-13 16:56:07 +0300

    Update README.md and add more usage examples.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# DotNetSamples
+# DotNetSamples #
+
+C# samples that are used in posts in my blog: <a href="http://automationrhapsody.com/">Automation Rhapsody</a>
+
+## SoftAssertions ##
+
+Code examples for post: <a href="http://automationrhapsody.com/soft-assertions-c-unit-testing-frameworks-mstest-nunit-xunit-net/">Soft assertions for C# unit testing frameworks (MSTest, NUnit, xUnit.net)</a>
--- SoftAssertions/UnitTest.cs
+++ SoftAssertions/UnitTest.cs
@@ -22,22 +22,30 @@ namespace SoftAssertions
         [TestMethod]
         public void TestPassingSoftAssertions()
         {
-            _softAssertions.Add("Passing Add assertion", true, true);
+            _softAssertions.Add("Passing bool Add assertion", true, true);
+            _softAssertions.Add("Passing string Add assertion", "SameString", "SameString");
+            _softAssertions.Add("Passing int Add assertion", 1, 1);
             _softAssertions.AddTrue("Passing AddTrue assertion", true);
         }
 
         [TestMethod]
-        public void TestFalingSoftAssertions()
+        public void TestFailingSoftAssertions()
         {
-            _softAssertions.Add("Failing Add assertion", true, false);
+            _softAssertions.Add("Failing bool Add assertion", true, false);
+            _softAssertions.Add("Failing string Add assertion", "SameString", "OtherString");
+            _softAssertions.Add("Failing int Add assertion", 1, 2);
             _softAssertions.AddTrue("Failing AddTrue assertion", false);
         }
 
         [TestMethod]
         public void TestMixedSoftAssertions()
         {
-            _softAssertions.Add("Passing Add assertion", true, true);
-            _softAssertions.Add("Failing Add assertion", true, false);
+            _softAssertions.Add("Passing bool Add assertion", true, true);
+            _softAssertions.Add("Failing bool Add assertion", true, false);
+            _softAssertions.Add("Passing string Add assertion", "SameString", "SameString");
+            _softAssertions.Add("Failing string Add assertion", "SameString", "OtherString");
+            _softAssertions.Add("Passing int Add assertion", 1, 1);
+            _softAssertions.Add("Failing int Add assertion", 1, 2);
             _softAssertions.AddTrue("Passing AddTrue assertion", true);
             _softAssertions.AddTrue("Failing AddTrue assertion", false);
         }
